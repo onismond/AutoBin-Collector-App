cd3bea0ee6bb3a826d89c6b2877dd9c5a86767c1
Onismond <67899826+onismond@users.noreply.github.com>
2025-01-09 22:26:54 +0000

    update

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,8 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyA2JmnQv7clr59b7aO4rt9clmPUQvMD1ZQ")
+    GeneratedPluginRegistrant.register(with: self)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/mech/eye_less.dart
+++ lib/mech/eye_less.dart
@@ -1,4 +1,4 @@
 class Confident {
   // Add your Google Maps API Key here
-  static const MAPS_API = 'AIzaSyCibZEJt4LNebP5Zw_zDTR1xW4_ci6pjCA';
+  static const MAPS_API = 'AIzaSyA2JmnQv7clr59b7aO4rt9clmPUQvMD1ZQ';
 }
